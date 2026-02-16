30e880a21e57d1b4a5c60534f372365de9de1cad
Mona Lisa <avishkajayashand@gmail.com>
2026-02-16 12:26:45 +0530

    added promises

--- app.js
+++ app.js
@@ -40,3 +40,21 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 }).on('error', (err) => {
   console.log("Error: " + err.message);
 });
+
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
